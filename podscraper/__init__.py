7ff24f2ece3fa0d8cb19d8a1cac6fd8dd68c2d3a
Justin Williams <justinw@me.com>
2017-05-20 09:04:39 -0600

    Make a class out of the Podcast Info Scraper

--- podscraper/__init__.py
+++ podscraper/__init__.py
@@ -1,6 +1,13 @@
 from categories import CategoryScraper
+from podcast_feeds import PodcastInfoScraper
 
 FILENAME = 'categories.csv'
 scraper = CategoryScraper(fileName=FILENAME)
 scraper.scrape()
 print("Done fetching and writing categories.")
+
+
+PODCASTS_FILE = 'podcasts.csv'
+scraper = PodcastInfoScraper(fileName=PODCASTS_FILE)
+scraper.scrape()
+print("Done parsing and writing RSS urls for each podcast.")
