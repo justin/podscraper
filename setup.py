0d693468f3bba3c5c3549b9f63e379246d76dce6
Justin Williams <justinw@me.com>
2017-05-20 08:40:50 -0600

    Reshuffling the deck chairs

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="podscraper",
+    version="0.0.1",
+    license="MIT",
+    author="Justin Williams",
+    author_email="justinw@me.com",
+    description="Convenience scripts to scrape the iTunes podcsast directory."
+)
