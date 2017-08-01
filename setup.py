0870855346b02e6919e3bd27649ed173d06c0180
Johnny Holton <johnny@holton.co>
2017-08-01 10:55:46 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="podscraper",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     author="Justin Williams",
     author_email="justinw@me.com",
